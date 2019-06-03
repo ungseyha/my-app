faf64d5928fa96703eef8e9e8875cbe93d9f6dd7
ungseyha <ung.seyha@gmail.com>
2019-06-03 14:12:30 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello world!</h1>
+     <h1 style ={{color:"blue"}}>Hello world!</h1>
+     <button>Click me!</button>
     </div>
   );
 }
